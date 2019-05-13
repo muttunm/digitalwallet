08de2af387a3d389101b8c28e413b57e64861b1b
muttunm <gsmuttunm@gmail.com>
2019-05-13 23:16:33 +0200

    initial commit

---
+++ src/main/java/com/hackthon/digitalwallet/mobilepay/MobilepayApplication.java
@@ -0,0 +1,13 @@
+package com.hackthon.digitalwallet.mobilepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilepayApplication.class, args);
+	}
+
+}
